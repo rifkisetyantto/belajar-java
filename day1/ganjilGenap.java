26411eb8a91c22450d2b5c13c0e3b1a3ff4b7d94
rifkid <rifki.dwi@batmandiri.com>
2020-01-21 09:47:13 +0700

    Pengenalan java

--- day1/ganjilGenap.java
+++ day1/ganjilGenap.java
@@ -22,7 +22,11 @@ public class ganjilGenap {
      System.out.println("================================");
      for (int i = 1; i <= 100; i++) {
          if (i%2 == 0) {
-             System.out.print(i+", ");
+             if (i==100) {
+                 System.out.println(i);
+             } else{
+                 System.out.print(i+", ");
+             }
          }
      }
      System.out.println();
@@ -31,9 +35,13 @@ public class ganjilGenap {
      System.out.println("================================");
      for (int i = 1; i <= 100; i++) {
          if (i%2 != 0) {
-             System.out.print(i+", ");
+            if (i==99) {
+                System.out.println(i);
+            } else{
+                System.out.print(i+", ");
+            }
          }
      }
-     
+     in.close();
     }
 }
